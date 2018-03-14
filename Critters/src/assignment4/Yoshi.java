921cd4fd40bdc5e24b264f0634f69f7ab07eb212
jenminsin <jenminsin@utexas.edu>
2018-03-14 17:13:32 -0500

    energy and reproduction
    changed the limits for energy boost and reproduction so it doesn't crash

--- Critters/src/assignment4/Yoshi.java
+++ Critters/src/assignment4/Yoshi.java
@@ -19,26 +19,26 @@ public class Yoshi extends TestCritter{
 	 //will move only diagonally 
 	public void doTimeStep() {
 		//less than 100 energy Yoshi gets 4 times his energy 
-		if(getEnergy() < 100) {
-			setEnergy(4*getEnergy());
+		if(getEnergy() < 50) {
+			setEnergy(2*getEnergy());
 		}
 		
-		if (getEnergy() > 100 && getEnergy() < 400 ) {
+		if (getEnergy() >= 100 && getEnergy() < 400 ) {
 			Yoshi pup1 = new Yoshi();
 			Yoshi pup2 = new Yoshi();
-			//will place pup to its current left 
-			reproduce(pup1, 4);
-			// will place pup to its current right
-			reproduce(pup2, 0);
+			
+			reproduce(pup1,getRandomInt(Params.number_of_directions ));
+			
+			reproduce(pup2, getRandomInt(Params.number_of_directions));
 		}
 		if (getEnergy() >=400) {
 			
 			Yoshi pup3 = new Yoshi();
 			Yoshi pup4 = new Yoshi();
-			//will place pup north
-			reproduce (pup3, 2);
-			//will place pup south
-			reproduce (pup4, 6);
+		
+			reproduce (pup3, getRandomInt(Params.number_of_directions));
+		
+			reproduce (pup4, getRandomInt(Params.number_of_directions));
 
 		}
 		
